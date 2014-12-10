b7e3d48e16a26ec8f2fc3e2d18b8f11821b9536f
AndyZheng <tianhua.zheng@theoutnet.cn>
2014-12-10 14:07:44 +0800

    db config

--- application/config/database.php
+++ application/config/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'access.conf';
+$db['default']['database'] = 'ci';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;

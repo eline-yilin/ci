3c65c62106f747b3111955dc710a62341cd32583
yjhcfzx <tianhua.zheng@theoutnet.cn>
2014-12-16 18:14:24 +0800

    login

---
+++ application/models/user_model.php
@@ -0,0 +1,105 @@
+<?php
+class user_model extends CI_Model {
+
+	var $main_table   = 'user';
+	var $content = '';
+	var $data    = array(
+			'id' => array(
+			'default'=> 0,
+			//'required'=>true,
+			'type'=>'int'		
+			),
+			'name' => array(
+			'required'=>true,
+			'type'=>'string'		
+			),
+			'username' => array(
+					'required'=>true,
+					'type'=>'string'
+			),
+			'password'=> array(
+					'type'=>'string'
+			),
+			'clientid'=> array(
+					'type'=>'int'
+			),
+			'birthday'=> array(
+					'type'=>'string'
+			),
+			'is_deleted'=> array(
+					'type'=>'int',
+					'default' => 0
+			)
+	);
+
+	function __construct()
+	{
+		// Call the Model constructor
+		parent::__construct();
+		$this->load->database('default');
+		$this->load->helper('db');
+	}
+	
+	function getList($param = null)
+	{
+		$query = $this->db->get_where('product', array('is_deleted'=>0));
+		$resp = array();
+		foreach ($query->result() as $row)
+		{
+			$resp[] = $row;
+		}
+		
+		return $resp;
+	}
+	
+ 
+	function login($username, $password)
+	{
+		$query = $this->db->get_where('user', array('username' => $username,'password' => $password,'is_deleted'=>0));
+		foreach ($query->result() as $row)
+		{
+			return $row;
+		}
+		return false;
+	}
+	
+	function getDetail($id)
+	{
+		$query = $this->db->get_where('entity', array('id' => $id,'is_deleted'=>0));
+		foreach ($query->result() as $row)
+		{
+			return $row;
+		}
+	}
+
+	function updateDetail($obj)
+	{
+		$request = my_process_db_request($obj, $this->data);
+		return $request;
+		
+
+		$this->db->update('entity', $request, array('id' => $_POST['id']));
+	}
+	
+	function createDetail($obj)
+	{
+
+		$request = my_process_db_request($obj, $this->data, false);
+
+		$request['id'] = null;
+		$this->db->insert('product', $request);
+		return $this->db->insert_id();
+		//return $obj;
+	}
+	
+	function deleteDetail($id)
+	{
+		return $id;
+		$this->title   = $_POST['title'];
+		$this->content = $_POST['content'];
+		$this->date    = time();
+	
+		$this->db->update('entries', $this, array('id' => $_POST['id']));
+	}
+}
+?>
